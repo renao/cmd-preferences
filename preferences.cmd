d527d219f0babe15b732fa12e9cdc6c0bf19250f
René Wernicke <rw@neopoly.de>
2016-05-09 10:56:46 +0200

    Add first cmd shell alias extensions

---
+++ preferences.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+%= some filthy linux familiarities =%
+doskey ls = dir /B
+
+%= git shortcuts =%
+doskey gb = git branch -v
+doskey gba = gb -a
+doskey gc = git commit -v
+doskey gco = git checkout
+doskey gca = gc -a
+doskey gd = git diff
+doskey gdc = git diff --cached
+doskey gl = git pull -v
+doskey glr = git pull -v --rebase
+doskey gg = git lg
+doskey gp = git push -v
+doskey grep = findstr /R /I /S $*
+doskey gst = git status -s
+doskey gfa = git fetch --all
+doskey gwc = git whatchanged -p --abbrev-commit --pretty=medium
+
+
+%= executables =%
+
+doskey adb = %USERPROFILE%\AppData\Local\android\sdk\platform-tools\adb.exe $*
+doskey subl = D:\sublime-text\sublime_text.exe $*
+
+%= folders =%
+
+doskey dev = cd %USERPROFILE%\devel\$*
+
+cd %USERPROFILE%\devel
